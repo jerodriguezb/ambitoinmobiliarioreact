09ea516a947f26187b3ba0094b4f9103279d04f0
Fabricio <fdimani1@gmail.com>
2021-06-12 18:36:41 -0300

    creando json server v2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import App from './App';
 
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+
+
 
 ReactDOM.render(
   <React.StrictMode>
