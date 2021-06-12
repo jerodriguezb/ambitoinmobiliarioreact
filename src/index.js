75bdc7a375bbdeec9308cff3e4439af25dab2cae
Fabricio <fdimani1@gmail.com>
2021-06-12 20:45:38 -0300

    funcionando chau json

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import App from './App';
+// import server from './server';
 
 // const jsonServer = require('json-server');
 // const server = jsonServer.create();
 // const router = jsonServer.router('db.json');
 // const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 3200;
+// const port = process.env.PORT || 3004;
 
 // server.use(middlewares);
 // server.use(router);
 
-// server.listen(port);
-
-
+// server.listen(port
+// server()
 
 ReactDOM.render(
   <React.StrictMode>
