0c01b2f984d2cefad8083f25a7eac1f608060e9c
Julio Rodriguez <jerodriguezb@gmail.com>
2021-06-16 17:01:04 -0300

    problemas con Principal.jsx Propiedades

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,118 +1,127 @@
-import React from 'react'
-import jwt_decode from 'jwt-decode'//Paquete para decodificar el Token
-import { Link, useLocation, useHistory } from 'react-router-dom'
+import React from "react";
+// import jwt_decode from 'jwt-decode'//Paquete para decodificar el Token
+import { Link, useLocation, useHistory } from "react-router-dom";
 
 // import { getCursos } from "../helpers/rutaCursos";
 // import { getPropiedades } from "../helpers/rutaPropiedades";
-import { Navbar, Nav, Button, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav, Button, NavDropdown } from "react-bootstrap";
 
-import '../css/navbar.css';
+import "../css/navbar.css";
 
 const NavBar = () => {
+  // //Defino location e history
+  // const location = useLocation();
+  // const history = useHistory();
 
+  // //estado para manejar el usuario
+  // const [user, setUser] = useState("Iniciar sesión");
 
-    //Defino location e history
-    const location = useLocation();
-    const history = useHistory();
+  // const [payload, setPayload] = useState({
+  //     role: "",
+  // });
 
-    //estado para manejar el usuario
-    const [user, setUser] = useState("Iniciar sesión");
+  // //estado para manejar los datos de los cursos
+  // const [cursos, setCursos] = useState({
+  //     data: {},
+  //     loading: true,
+  // });
 
-    const [payload, setPayload] = useState({
-        role: "",
-    });
+  // //Si cambia la locación asigno a user el valor de localstorage
+  // useEffect(() => {
+  //     setUser(JSON.parse(localStorage.getItem("usuario")) || "Iniciar Sesión");
+  //     ActualizarData();
+  //     checkToken();
+  // }, [location]);
 
-    //estado para manejar los datos de los cursos
-    const [cursos, setCursos] = useState({
-        data: {},
-        loading: true,
-    });
+  // //Cuando monto navbar se cargan los cursos
+  // useEffect(() => {
+  //     ActualizarData();
+  //     // getCursos().then((datos) => {
+  //     //   setCursos({
+  //     //     data: datos,
+  //     //     loading: false,
+  //     //   });
+  //     // });
+  // }, []);
 
-    //Si cambia la locación asigno a user el valor de localstorage
-    useEffect(() => {
-        setUser(JSON.parse(localStorage.getItem("usuario")) || "Iniciar Sesión");
-        ActualizarData();
-        checkToken();
-    }, [location]);
+  // const ActualizarData = () => {
+  //     getCursos(0, 100).then((datos) => {
+  //         setCursos({
+  //             data: datos,
+  //             loading: false,
+  //         });
+  //     });
+  // };
 
-    //Cuando monto navbar se cargan los cursos
-    useEffect(() => {
-        ActualizarData();
-        // getCursos().then((datos) => {
-        //   setCursos({
-        //     data: datos,
-        //     loading: false,
-        //   });
-        // });
-    }, []);
+  // //Manejo el deslogueo de la web
+  // const handleLogin = () => {
+  //     localStorage.setItem("token", JSON.stringify(""));
+  //     localStorage.setItem("id", JSON.stringify(""));
+  //     localStorage.setItem("usuario", JSON.stringify("Iniciar Sesión"));
+  //     setUser(JSON.parse(localStorage.getItem("usuario")));
+  //     setPayload({ role: "" });
+  //     history.push("/login");
+  // };
 
-    const ActualizarData = () => {
-        getCursos(0, 100).then((datos) => {
-            setCursos({
-                data: datos,
-                loading: false,
-            });
-        });
-    };
+  // const checkToken = () => {
+  //     let token = JSON.parse(localStorage.getItem("token")) || "";
+  //     if (token.length > 0) {
+  //         let token_decode = jwt_decode(localStorage.getItem("token")); //Obteniendo los datos del payload
+  //         setPayload(token_decode.usuario);
+  //     }
+  // };
 
-    //Manejo el deslogueo de la web
-    const handleLogin = () => {
-        localStorage.setItem("token", JSON.stringify(""));
-        localStorage.setItem("id", JSON.stringify(""));
-        localStorage.setItem("usuario", JSON.stringify("Iniciar Sesión"));
-        setUser(JSON.parse(localStorage.getItem("usuario")));
-        setPayload({ role: "" });
-        history.push("/login");
-    };
+  return (
+    <div id="navBar" className="navBar">
+      <Navbar bg="light" expand="lg">
+        <img
+          src="https://lh3.googleusercontent.com/l72waZuMNi55w1PiR0cqa1SYPWhxuuIGOY6r2WEvXeCMI9w-u719PUQBSR0c6NvL9v8wptDvxIHW32voNWZy3L6vY7dcuavXZysjirhzRr9a4g_s0pw_QzE-0NyK-VLsVZfcIT1tJQ=w2400"
+          alt="logo"
+        />
+        <Link claseName="nav" to="/">
+          <Navbar.Brand>Ambito Inmobiliaria</Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* <Navbar className="mr-auto"> */}
+          <Nav>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/propiedades" className="nav-link1 text-decoration-none">
+              Buscar Propiedades
+            </Link>
+            <Link
+              to={{
+                pathname:
+                  "https://docs.google.com/forms/d/e/1FAIpQLScTpePk0bQ3HVpbFP-UCRkmYs6Smkl3_Ck6QT6v9SSG5M8LOg/viewform",
+              }}
+              className="nav-link"
+              target="_blank"
+            >
+              Contactanos
+            </Link>
+            <Link to="/" className="nav-link">
+              Quienes Somos
+            </Link>
+            <Link to="/" className="nav-link"></Link>
+          </Nav>
 
-    const checkToken = () => {
-        let token = JSON.parse(localStorage.getItem("token")) || "";
-        if (token.length > 0) {
-            let token_decode = jwt_decode(localStorage.getItem("token")); //Obteniendo los datos del payload
-            setPayload(token_decode.usuario);
-        }
-    };
+          {/* {payload.role === "ADMIN_ROLE" && (
+            <Link to="/admin" className="text-decoration-none text-muted mr-2">
+              Administrador
+            </Link>
+          )}
+          <button className="btn btn-outline-info" onClick={handleLogin}>
+            {user}
+          </button> */}
 
+          {/* </Navbar> */}
+        </Navbar.Collapse>
+      </Navbar>
+      <h4>Contacto: Benjamin Neville +54 381-6900030</h4>
+    </div>
+  );
+};
 
-
-
-
-    return (
-
-        <div id="navBar" className="navBar">
-
-            <Navbar bg="light" expand="lg">
-                <img src="https://lh3.googleusercontent.com/l72waZuMNi55w1PiR0cqa1SYPWhxuuIGOY6r2WEvXeCMI9w-u719PUQBSR0c6NvL9v8wptDvxIHW32voNWZy3L6vY7dcuavXZysjirhzRr9a4g_s0pw_QzE-0NyK-VLsVZfcIT1tJQ=w2400"
-                    alt="logo" />
-                <Link claseName="nav" to="/"><Navbar.Brand>Ambito Inmobiliaria</Navbar.Brand></Link>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    {/* <Navbar className="mr-auto"> */}
-                    <Nav>
-                        <Link to="/" className="nav-link">Home</Link>
-                        <Link to="/propiedades" className="nav-link1 text-decoration-none">Buscar Propiedades</Link>
-                        <Link to={{ pathname: "https://docs.google.com/forms/d/e/1FAIpQLScTpePk0bQ3HVpbFP-UCRkmYs6Smkl3_Ck6QT6v9SSG5M8LOg/viewform" }} className="nav-link" target="_blank">Contactanos</Link>
-                        <Link to="/" className="nav-link">Quienes Somos</Link>
-                        <Link to="/" className="nav-link"></Link>
-
-                    </Nav>
-                    
-                    {payload.role === "ADMIN_ROLE" && (
-                        <Link to="/admin" className="text-decoration-none text-muted mr-2">
-                            Administrador
-                        </Link>
-                    )}
-                    <button className="btn btn-outline-info" onClick={handleLogin}>
-                        {user}
-                    </button>
-
-
-                    {/* </Navbar> */}
-                </Navbar.Collapse>
-            </Navbar>
-            <h4>Contacto: Benjamin Neville +54 381-6900030</h4>
-        </div>
-    )
-}
-
-export default NavBar
+export default NavBar;
--- src/helpers/rutaPropiedades.js
+++ src/helpers/rutaPropiedades.js
@@ -1,25 +1,50 @@
 import axios from "axios";
+import qs from "qs";
 
+//Traer todos los cursos con el limite y desde que registro
+export const getPropiedades = async (desde = 0, limite = 50) => {
+  let url = `http://localhost:3004/propiedades?desde=${desde}&limite=${limite}`;
 
-export const getPropiedades = async () => {
-  const resp = await axios("http://localhost:3004/propiedades");
+  const options = {
+    method: "GET",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  };
+  try {
+    const resp = await axios(url, options);
+    const { data } = resp;
 
-  // console.log(resp);
-
-  const { data } = resp;
-
-  return data;
+    return data;
+  } catch (error) {
+    return {
+      data: error.response.data,
+      loading: false,
+    };
+  }
 };
 
-export const getPropiedadId= async (id)=>{
-  const resp = await axios(`http://localhost:3004/propiedades/${id}`);
-
- 
-  const { data } = resp;
-
-  return data;
-
-}
+//Traer un curso según su id
+export const getPropiedadId = async (id) => {
+  let url = `http://localhost:3004/propiedades/${id}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  };
+  try {
+    const resp = await axios(url, options);
+    const { data } = resp;
+    console.log(data);
+    return data;
+  } catch (error) {
+    return {
+      data: error.response.data,
+      loading: false,
+    };
+  }
+};
 
 export const getCarousel = async () => {
   const resp = await axios("http://localhost:3004");
@@ -35,3 +60,84 @@ export const getMarquesina = async () => {
   return data;
 };
 
+//Crear nuevo curso
+// export const addPropiedad = async (datos) => {
+//   // console.log(datos);
+//   const token = JSON.parse(localStorage.getItem("token")) || "";
+//   let url = "http://localhost:3004/cursos";
+
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "content-type": "application/x-www-form-urlencoded",
+//       token: token,
+//     },
+//     data: qs.stringify(datos),
+//   };
+//   try {
+//     const resp = await axios(url, options);
+//     const { data } = resp;
+//     console.log(resp);
+//     return data;
+//   } catch (error) {
+//     // console.log(error.response.data);
+//     return {
+//       data: error.response.data,
+//       loading: false,
+//     };
+//   }
+// };
+
+//Actualizar curso
+// export const modifCurso = async (datos, id) => {
+//   // console.log(datos);
+//   const token = JSON.parse(localStorage.getItem("token")) || "";
+//   let url = `http://localhost:3004/cursos/${id}`;
+
+//   const options = {
+//     method: "PUT",
+//     headers: {
+//       "content-type": "application/x-www-form-urlencoded",
+//       token: token,
+//     },
+//     data: qs.stringify(datos),
+//   };
+//   try {
+//     const resp = await axios(url, options);
+//     const { data } = resp;
+//     console.log(data);
+//     return data;
+//   } catch (error) {
+//     console.log(error.response.data);
+//     return {
+//       data: error.response.data,
+//       loading: false,
+//     };
+//   }
+// };
+
+//Inactivar un curso
+// export const delCurso = async (id) => {
+//   const token = JSON.parse(localStorage.getItem("token")) || "";
+//   let url = `http://localhost:3004/cursos/${id}`;
+
+//   const options = {
+//     method: "DELETE",
+//     headers: {
+//       "content-type": "application/x-www-form-urlencoded",
+//       token: token,
+//     },
+//   };
+//   try {
+//     const resp = await axios(url, options);
+//     const { data } = resp;
+//     // console.log(data);
+//     return data;
+//   } catch (error) {
+//     // console.log(error.response.data);
+//     return {
+//       data: error.response.data,
+//       loading: false,
+//     };
+//   }
+// };
